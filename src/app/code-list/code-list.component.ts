75ec7f8c46e59b4cc59827a0f5129ba700a0d26e
Matthew Day <matt@matthewday.net>
2019-03-16 11:37:25 -0400

    Add html markup to show mock data for list

--- src/app/code-list/code-list.component.ts
+++ src/app/code-list/code-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import * as Data from './../../mocks/codes.json';
 
 @Component({
   selector: 'app-code-list',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CodeListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public breakpointObserver: BreakpointObserver) { }
 
   ngOnInit() {
+    this.breakpointObserver
+    .observe(['(min-width: 500px)'])
+    .subscribe((state: BreakpointState) => {
+      if(state.matches) {
+        this.viewport500 = true;
+      } else {
+        this.viewport500 = false;
+      }
+    });
   }
+
+  @Output() broadcastDetailsBind = new EventEmitter<string>();
+
+  viewport500 = false;
+
+  selectCode = (code) => {
+    this.broadcastDetailsBind.emit(code);
+  }
+
+  codes = Data.codes;
 
 }
