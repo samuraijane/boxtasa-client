6cb7e7c7651c0c283c24eb28d3446ebcc690c01a
Matthew Day <matt@matthewday.net>
2019-03-16 13:16:33 -0400

    Add style at app-level and output components at app-level

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, isDevMode, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'boxtasa';
+export class AppComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+    if (isDevMode()) {
+      console.log('👋 Development!');
+    } else {
+      console.log('💪 Production!');
+    }
+  }
+
+  selectedCode: string = null;
+  toggle = (code) => {
+    this.selectedCode = code;
+  }
+
 }
