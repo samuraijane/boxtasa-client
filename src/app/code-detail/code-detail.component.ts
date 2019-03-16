2ba5297f8d41d1b67554ebc75bab88d4748aebae
Matthew Day <matt@matthewday.net>
2019-03-16 13:16:58 -0400

    Style CodeDetail component

--- src/app/code-detail/code-detail.component.ts
+++ src/app/code-detail/code-detail.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { Code } from '../interfaces';
 
 @Component({
   selector: 'app-code-detail',
   templateUrl: './code-detail.component.html',
-  styleUrls: ['./code-detail.component.scss']
+  styleUrls: ['./code-detail.component.scss'],
+  encapsulation: ViewEncapsulation.None // TODO research this further
 })
 export class CodeDetailComponent implements OnInit {
 
   constructor() { }
 
+  data: Code;
+
+  @Input() receiveDetailsBind: Code;
+  @Output() cancelDetailsBind = new EventEmitter<string>();
+
   ngOnInit() {
+    this.data = this.receiveDetailsBind;
+  }
+
+  cancelModal = () => {
+    this.cancelDetailsBind.emit(null);
   }
 
 }
