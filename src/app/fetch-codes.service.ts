2a5bbbe6a1c3c703b2d444cf684108cfe11cabaf
Matthew Day <matt.day@shakerinternational.com>
2019-09-30 06:16:59 -0400

    Update server URL address

--- src/app/fetch-codes.service.ts
+++ src/app/fetch-codes.service.ts
@@ -1,23 +1,23 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Observable } from 'rxjs';
-import { map, debounceTime, switchMap } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { Observable } from "rxjs";
+import { map, debounceTime, switchMap } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class FetchCodesService {
   // TODO consider replacing this with an injector token (see https://blog.thoughtram.io/angular/2016/05/23/opaque-tokens-in-angular-2.html)
   // baseUrl: string = 'http://localhost:3001/codes';
-  baseUrl: string = 'http://agile-basin-38660.herokuapp.com/codes';
+  baseUrl: string = "https://sj-boxtasa-server.herokuapp.com/codes";
 
-  constructor(private http: Http) { }
+  constructor(private http: Http) {}
 
   search() {
-    return this.http
-      .get(`${this.baseUrl}`)
-      .pipe(map(res => {
+    return this.http.get(`${this.baseUrl}`).pipe(
+      map(res => {
         return res.json();
-      }));
+      })
+    );
   }
 }
