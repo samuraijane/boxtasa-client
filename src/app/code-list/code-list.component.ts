9519abee0f51e64f19edfc7cfd228e22d3b23f0f
Matthew Day <matt.day@shakerinternational.com>
2020-02-04 18:38:34 -0500

    Sort retrieved codes

--- src/app/code-list/code-list.component.ts
+++ src/app/code-list/code-list.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit, Output } from '@angular/core';
-import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
-import { Subject } from 'rxjs';
+import { Component, OnInit, Output } from "@angular/core";
+import { BreakpointObserver, BreakpointState } from "@angular/cdk/layout";
+import { Subject } from "rxjs";
 
-import { Code } from './../interfaces';
-import { DetailsService } from './../details.service';
-import { FetchCodesService } from './../fetch-codes.service';
+import { Code } from "./../interfaces";
+import { DetailsService } from "./../details.service";
+import { FetchCodesService } from "./../fetch-codes.service";
 
 @Component({
-  selector: 'app-code-list',
-  templateUrl: './code-list.component.html',
-  styleUrls: ['./code-list.component.scss'],
+  selector: "app-code-list",
+  templateUrl: "./code-list.component.html",
+  styleUrls: ["./code-list.component.scss"],
   providers: [DetailsService, FetchCodesService]
 })
 export class CodeListComponent implements OnInit {
-
   _listFilter: string;
 
   get listFilter(): string {
@@ -21,18 +20,20 @@ export class CodeListComponent implements OnInit {
   }
   set listFilter(value: string) {
     this._listFilter = value;
-    this.filteredCodes = this.listFilter ? this.performFilter(this.listFilter) : this.codes;
+    this.filteredCodes = this.listFilter
+      ? this.performFilter(this.listFilter)
+      : this.codes;
   }
 
   performFilter(filterBy: string) {
     filterBy = filterBy.toLocaleLowerCase();
-    return this.codes.filter((code) => {
+    return this.codes.filter(code => {
       let value;
       code.sampleNames.forEach((item, index) => {
-        if(item.toLocaleLowerCase().indexOf(filterBy) !== -1) {
+        if (item.toLocaleLowerCase().indexOf(filterBy) !== -1) {
           value = item.toLocaleLowerCase().indexOf(filterBy) !== -1;
         }
-      })
+      });
       return value;
     });
   }
@@ -45,26 +46,29 @@ export class CodeListComponent implements OnInit {
 
   ngOnInit(): void {
     this.breakpointObserver
-    .observe(['(min-width: 500px)'])
-    .subscribe((state: BreakpointState) => {
-      if(state.matches) {
-        this.viewport500 = true;
-      } else {
-        this.viewport500 = false;
-      }
-    });
+      .observe(["(min-width: 500px)"])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.viewport500 = true;
+        } else {
+          this.viewport500 = false;
+        }
+      });
 
-    this.fetchCodesService.search()
-    .subscribe(
+    this.fetchCodesService.search().subscribe(
       results => {
-        this.codes = results;
+        this.codes = results.sort((a, b) => {
+          if (a.title < b.title) return -1;
+          if (a.title > b.title) return 1;
+          return 0;
+        });
         this.filteredCodes = this.codes;
       },
-      error => this.errorMessage = <any>error
+      error => (this.errorMessage = <any>error)
     );
 
     this.detailsService.detailsStream$.subscribe(detail => {
-      if(!detail) {
+      if (!detail) {
         this.filteredCodes = this.codes;
       }
       this.codeDetails = detail;
@@ -73,13 +77,13 @@ export class CodeListComponent implements OnInit {
 
   viewport500 = false;
 
-  selectCode = (code) => {
+  selectCode = code => {
     this.detailsService.setDetails(code);
     this.filteredCodes = [code];
-  }
+  };
 
   codes: Array<Code> = [];
   codeDetails: Code;
-  errorMessage: string = '';
+  errorMessage: string = "";
   filteredCodes: Array<Code> = [];
 }
